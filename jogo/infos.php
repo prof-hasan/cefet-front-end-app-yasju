dae083db328f321bad07d7f7afffba693314b77f
JoaoPaulo907 <joaopfsuarez@gmail.com>
2025-12-13 20:18:10 -0300

    Add files via upload

---
+++ jogo/infos.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Sobre a Página</title>
+        <link rel="stylesheet" href="estilos/infos.css">
+        <link rel="icon" href="imgs/icone.png">
+    </head>
+
+    <body>
+        <header>
+            <h1>Sobre a Página</h1>
+        </header>
+
+        <main>
+
+
+        </main>
+
+        <script src="//cdn.rawgit.com/namuol/cheet.js/master/cheet.min.js" type="text/javascript"></script>
+        <script src=""></script>
+    </body>
+</html>
